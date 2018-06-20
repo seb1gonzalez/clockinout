2ab0d8c07f64627ea67f63987207ca411006cc00
ulises <jorgehuertahu@gmail.com>
2018-06-20 08:50:08 -0600

    new file: inserting ip into db

---
+++ exterior.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', 30000); //300 seconds = 5 minutes
+
+$conn = mysqli_connect('ctis.utep.edu', 'ctis', '19691963', 'clockinout');
+
+//$toReturn = array();
+
+//getters
+//ip
+//id
+
+
+//insert to db
+//done
+
+header('Content-Type: application/json');
+//echo json_encode($toReturn);
+$conn->close();
+?>
