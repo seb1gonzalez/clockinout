8433f319c8b17e66a9126728e797eedc5d9310a2
ulises <jorgehuertahu@gmail.com>
2018-06-20 11:29:08 -0600

    edits on exterior

--- exterior.php
+++ exterior.php
@@ -4,19 +4,24 @@ ini_set('max_execution_time', 30000); //300 seconds = 5 minutes
 
 $conn = mysqli_connect('ctis.utep.edu', 'ctis', '19691963', 'clockinout');
 
-//$toReturn = array();
+$toReturn = array();
 
 //ipad is 129.108.202.162, as well as other in utep secure
 
 //getters
-//ip
-//id
-
+$id = $_GET['id'];//id
+$ip = $_GET['ip'];//ip
+$info = $_GET['info'];//info
 
 //insert to db
 //done
 
+array_push($toReturn, $id);
+array_push($toReturn, $ip);
+array_push($toReturn, $info);
+
+
 header('Content-Type: application/json');
-//echo json_encode($toReturn);
+echo json_encode($toReturn);
 $conn->close();
 ?>
