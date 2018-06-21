51e9789779a206e88506946f294115a804d5b9e6
ulises <jorgehuertahu@gmail.com>
2018-06-21 11:29:09 -0600

    php and html to display exterior logins

--- exterior.php
+++ exterior.php
@@ -8,14 +8,24 @@ $toReturn = array();
 
 //ipad is 129.108.202.162, as well as other in utep secure
 
+function fetchAll($r){
+    $temp = array();
+    while($row = mysqli_fetch_assoc($r)){
+        $temp[] = $row;
+    }
+    return $temp;
+}
+
 //getters
 $id = $_GET['id'];
 $ip = $_GET['ip'];
 $info = $_GET['info'];
 
-$query = "insert  into exterior_logs (id, ip, info) values($id, $ip, $info)";
+$query = "insert into exterior_logs (id, ip, info) values($id, $ip, $info)";
 $result = mysqli_query($conn, $query);
+$result = fetchAll($result);
 
+$toReturn = $result;
 
 header('Content-Type: application/json');
 echo json_encode($toReturn);
