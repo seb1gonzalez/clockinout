c81514f2fe984f52a74277561f37930bc23e4a49
ulises <jorgehuertahu@gmail.com>
2018-06-28 10:11:05 -0600

    ready to review

--- js/clockinout.js
+++ js/clockinout.js
@@ -74,6 +74,7 @@ function timeTable(){
 
     $('#users-in tbody tr').remove();
     $('#users-names tbody tr').remove();
+    $('#users-numbers tbody tr').remove();
     $.getJSON("backend/public/api/usersLogs", function (usersin) {
         for (var i = 0; i < usersin.length; i++) {
             button = '';
